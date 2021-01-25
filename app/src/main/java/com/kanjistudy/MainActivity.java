98fa1c7614c08a12c14ff4a70a71a8af30b63af9
sergioparamo <sergio.paramo1997@gmail.com>
2021-01-25 14:26:18 +0100

    first commit

---
+++ app/src/main/java/com/kanjistudy/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kanjistudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
